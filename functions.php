ab8509cf032ab0e41cd8bff2feffc045decbcca7
Jacob DeBenedetto <deben3@gmail.com>
2020-09-29 10:49:33 -0700

    add featured post and styling

--- functions.php
+++ functions.php
@@ -2,12 +2,17 @@
 	// Child theme functions
 	function my_et_enqueue_styles() { wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); }
 
-	// Initialize core class objects
+	// Load PHP libraries
+	require_once 'php/class-resources.php';
 	require_once 'php/class-shortcodes.php';
 	require_once 'php/class-blog.php';
 
 	// Enqueue child theme information
 	add_action( 'wp_enqueue_scripts', 'my_et_enqueue_styles' );
+
+	// Initialize classes
+	$six_resources = new Six_Resources();
+	$six_shortcodes = new Six_Shortcodes();
 
 	// Apply actions and filters
 	add_filter('init', 'Six_Shortcodes::add_shortcode'); // Add shortcode object
---
+++ php/class-resources.php
@@ -0,0 +1,11 @@
+<?php
+    class Six_Resources {
+        public function __construct() {
+            if (!is_admin()) {
+                // Register scripts and stylesheets
+                wp_register_script('featured-post', get_stylesheet_directory_uri() . '/js/featured-post.js', array(), time());
+                wp_register_style('featured-post', get_stylesheet_directory_uri() . '/css/featured-post.css', array(), time());
+            }
+        }
+    }
+?>
