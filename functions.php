ec0a8b42de8f838024e270ba9bd0daf0a6731cd0
doppl3r <deben3@gmail.com>
2021-02-15 16:42:54 -0700

    create endpoint for user data

--- functions.php
+++ functions.php
@@ -1,20 +1,24 @@
 <?php
 	// Child theme functions
 	function my_et_enqueue_styles() { wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); }
+	function add_cors_http_header(){ header("Access-Control-Allow-Origin: *"); }
 
 	// Load PHP libraries
 	require_once 'php/class-resources.php';
 	require_once 'php/class-shortcodes.php';
 	require_once 'php/class-blog.php';
+	require_once 'php/class-user.php';
 
-	// Enqueue child theme information
-	add_action( 'wp_enqueue_scripts', 'my_et_enqueue_styles' );
+	// Apply actions
+	add_action('init','add_cors_http_header');
+	add_action('wp_enqueue_scripts', 'my_et_enqueue_styles');
+	add_action('rest_api_init', 'Six_User::add_rest_route');
 
 	// Initialize classes
 	$six_resources = new Six_Resources();
 	$six_shortcodes = new Six_Shortcodes();
 
-	// Apply actions and filters
+	// Apply filters
 	add_filter('the_content', 'Six_Blog::update_content', 1); // Update post content
 	add_filter('init', 'Six_Shortcodes::add_shortcode'); // Add shortcode object
 ?>
---
+++ php/class-user.php
@@ -0,0 +1,34 @@
+<?php
+    class Six_User {
+            
+        public function __construct() {
+            if (!is_admin()) {
+                
+            }
+        }
+
+        // Add rest route
+        public function add_rest_route() {
+            // Endpoint: https://www.six-degrees.com/wp-json/user/data
+            register_rest_route('user', 'data', array(
+                'methods'  => array('POST', 'GET'),
+                'callback' => 'Six_User::get_user_request'
+            ));
+        }
+
+        // Parse levels request
+        public function get_user_request($request) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $geo = json_decode(file_get_contents('http://www.geoplugin.net/json.gp?ip=' . $ip));
+            $data = array(
+                'ip' => $ip,
+                'city' => $geo->geoplugin_city,
+                'state' => $geo->geoplugin_regionName,
+                'country' => $geo->geoplugin_countryName,
+                'continent' => $geo->geoplugin_continentName,
+                'latitude' => $geo->geoplugin_latitude,
+                'longitude' => $geo->geoplugin_longitude
+            );
+            return wp_send_json($data);
+        }
+    }
